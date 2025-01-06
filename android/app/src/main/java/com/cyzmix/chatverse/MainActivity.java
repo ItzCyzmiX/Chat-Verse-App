86b00da68b33cafcb34d287ea07eb9e19c4bc44f
CyzmiX <itzmedigamingx@gmail.com>
2025-01-06 21:37:57 +0100

    capacitor app

---
+++ android/app/src/main/java/com/cyzmix/chatverse/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cyzmix.chatverse;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // New import
+export default {
+  kit: {
+    adapter: adapter({
+      // Optional: Specify output directory (default is 'build')
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html', // Required for SPA mode
+    }),
+  },
+  preprocess: vitePreprocess(),
 };
-
-export default config;
